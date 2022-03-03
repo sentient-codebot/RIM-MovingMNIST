60e0f3671c331f13b2f03281a90859c1286e4bac
Nan <nanlin1997@outlook.com>
2022-03-03 19:42:19 +0100

    adjust bash files

--- experiment_mmnist_gdrop_ten.sh
+++ experiment_mmnist_gdrop_ten.sh
@@ -10,6 +10,6 @@ save_freq=25
 version=0
 num_units=10
 k=3
-rim_dropout=0.2
+rim_dropout=0.3
 
 python3 train_mmnist.py --experiment_name $experiment_name --cfg_json $cfg_json --core $core --should_resume $should_resume --model_persist_frequency $save_freq --version $version --num_units $num_units --k $k --rim_dropout $rim_dropout
