8bf45866b46b003138556dfd435410c20ad76a34
Nan <nanlin1997@outlook.com>
2022-04-01 10:37:53 +0200

    [ADD] bash for increasing module exp

--- experiment_mmnist_slotrim.sh
+++ experiment_mmnist_slotrim.sh
@@ -4,12 +4,13 @@ echo Running on $HOSTNAME
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate pytorch
 
-experiment_name="MMNIST_slotrim_final_activation"
+experiment_name="MMNIST_slotrim_full"
 cfg_json="configs/rim_complete/rim_complete.json"
 core="RIM"
 should_resume="false"
 save_freq=25
+num_units=3
 k=3
 bs=32
 
-python3 train_mmnist.py --experiment_name $experiment_name --cfg_json $cfg_json --core $core --should_resume $should_resume --model_persist_frequency $save_freq --k $k --batch_size $bs 
+python3 train_mmnist.py --experiment_name $experiment_name --cfg_json $cfg_json --core $core --should_resume $should_resume --model_persist_frequency $save_freq --k $k --batch_size $bs --num_units $num_units
