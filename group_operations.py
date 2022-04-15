2150c45b68e232e61d90bea15e8fb29edc5b06a4
Nan <nanlin1997@outlook.com>
2022-04-15 13:37:58 +0200

    [ADD] added SharedWorkspace to group_ops.py

--- group_operations.py
+++ group_operations.py
@@ -178,3 +178,112 @@ class GroupDropout(nn.Module):
             return hidden*rnd_mask*compensate
 
         return hidden
+
+class SharedWorkspace(nn.Module):
+    """An implementation of "Coordination Among Neural Modules Through a Shared Global Workspace"(https://arxiv.org/abs/2103.01197) 
+
+    Memory update is implemented with a skip connection, which is different than original methodology. 
+
+    Args:
+        `write_key_size`: attention key size for the memory writing phase
+        `read_key_size`: attention key size for the broadcast and hidden state updating phase
+        `memory_size`: memory slot size
+        `hidden_size`: hidden state vector size
+        `write_num_heads`: number of attention heads for write phase
+        `read_num_heads`: number of attention heads for read phase
+        `write_value_size`: value size for the memory writing phase
+        `read_value_size`: value size for the broadcast and hidden state updating phase
+        `write_dropout`: dropout probability for the memory writing phase
+        `read_dropout`: dropout probability for the broadcast and hidden state updating phase
+
+    Inputs:
+        `M`: current memory slots of shape [N, K_mem, D_mem]
+        `h`: hidden state vectors of shape [N, K_hidden, D_hidden] 
+        `mask`: [optional] activation mask, select which hidden state vectors has write access to the memory slots, of shape [N, K_hidden]
+        
+    Output:
+        `M_new`: updated memory slots
+        `h`: updated hidden state vectors by broadcast memory slots """
+    def __init__(self, write_key_size, read_key_size, memory_size, hidden_size, write_num_heads=1, read_num_heads=1, write_value_size=None, read_value_size=None, write_dropout=0.0, read_dropout=0.0):
+        super().__init__()
+        self.write_key_size = write_key_size
+        self.read_key_size = read_key_size
+        self.memory_size = memory_size
+        self.hidden_size = hidden_size
+        self.write_num_heads = write_num_heads
+        self.read_num_heads = read_num_heads
+        self.write_value_size = write_value_size
+        self.read_value_size = read_value_size
+        self.write_dropout = write_dropout
+        self.read_dropout = read_dropout
+        if write_num_heads>1:
+            assert write_value_size is not None 
+        else:
+            self.write_value_size = memory_size
+        if read_num_heads>1:
+            assert read_value_size is not None
+        else: 
+            self.read_value_size = hidden_size
+        self.write_key_transform = nn.Linear(self.hidden_size, self.write_num_heads * self.write_key_size, bias=False) # hidden -> write_key
+        self.write_value_transform = nn.Linear(self.hidden_size, self.write_num_heads * self.write_value_size, bias=False) # hidden -> write_value
+        self.write_query_transform = nn.Linear(self.memory_size, self.write_num_heads * self.write_key_size, bias=False) # memory -> write_query
+        self.write_output_transform = nn.Linear(self.write_num_heads * self.write_value_size, self.memory_size, bias=False) # write_value -> write_output
+        self.write_dropout = nn.Dropout(self.write_dropout)
+
+        self.read_key_transform = nn.Linear(self.memory_size, self.read_num_heads * self.read_key_size, bias=False) # memory -> read_key
+        self.read_value_transform = nn.Linear(self.memory_size, self.read_num_heads * self.read_value_size, bias=False) # memory -> read_value
+        self.read_query_transform = nn.Linear(self.hidden_size, self.read_num_heads * self.read_key_size, bias=False) # hidden -> read_query
+        self.read_output_transform = nn.Linear(self.read_num_heads * self.read_value_size, self.hidden_size, bias=False) # read_value -> read_output
+        self.read_dropout = nn.Dropout(self.read_dropout)
+
+    def forward(self, M, h, mask=None):
+        """
+        Inputs:
+            `M`: current memory slots of shape [N, K_mem, D_mem]
+            `h`: hidden state vectors of shape [N, K_hidden, D_hidden] 
+            `mask`: [optional] activation mask, select which hidden state vectors has write access to the memory slots, of shape [N, K_hidden]
+            
+        Output:
+            `M_new`: updated memory slots
+            `h_new`: updated hidden state vectors by broadcast memory slots """
+
+        # Memory Writing Phase
+        if mask is not None:
+            assert mask.dim() == 2 or mask.dim() == 3
+            if mask.dim() == 2:
+                mask = mask.unsqueeze(2) # Shape [N, K_hidden, 1]
+            mask = mask.detach() # no grad 
+            h = h*mask
+        write_key = self.write_key_transform(h) # Shape [N, K_hidden, write_num_heads * write_key_size]
+        write_value = self.write_value_transform(h) # Shape [N, K_hidden, write_num_heads * D_mem]
+        write_query = self.write_query_transform(M) # Shape [N, K_mem, write_num_heads * write_key_size]
+        write_key = write_key.view(write_key.shape[0], self.write_num_heads, write_key.shape[1], self.write_key_size) # Shape [N, heads, K_hidden, write_key_size]
+        write_value = write_value.view(write_value.shape[0], self.write_num_heads, write_value.shape[1], self.write_value_size) # Shape [N, heads, K_hidden, write_value_size]
+        write_query = write_query.view(write_query.shape[0], self.write_num_heads, write_query.shape[1], self.write_key_size) # Shape [N, heads, K_mem, write_key_size]
+        score_write = torch.matmul(write_query, write_key.transpose(-1,-2)) / math.sqrt(self.write_key_size) # Shape [N, heads, K_mem, K_hidden]
+        score_write = nn.Softmax(dim=3)(score_write) # Shape [N, heads, K_mem, K_hidden] 
+        mem_update = torch.matmul(score_write, write_value) # Shape [N, heads, K_mem, write_value_size]
+        mem_update = mem_update.transpose(1, 2).flatten(start_dim=-2) # Shape [N, K_mem, heads * write_value_size]
+        mem_update = self.write_output_transform(self.write_dropout(mem_update)) # Shape [N, K_mem, D_mem]
+
+        M_new = M + mem_update # Shape [N, K_mem, D_mem]
+
+        # Memory Broadcast Phase and Hidden State Update Phase (Read Phase)
+        read_key = self.read_key_transform(M_new) # Shape [N, K_mem, read_key_size]
+        read_value = self.read_value_transform(M_new) # Shape [N, K_mem, D_hidden]
+        read_query = self.read_query_transform(h) # Shape [N, K_hidden, read_key_size]
+        read_key = read_key.view(read_key.shape[0], self.read_num_heads, read_key.shape[1], self.read_key_size) # Shape [N, heads, K_mem, read_key_size]
+        read_value = read_value.view(read_value.shape[0], self.read_num_heads, read_value.shape[1], self.read_value_size) # Shape [N, heads, K_mem, read_value_size]
+        read_query = read_query.view(read_query.shape[0], self.read_num_heads, read_query.shape[1], self.read_key_size) # Shape [N, heads, K_hidden, read_key_size]
+        score_read = torch.matmul(read_query, read_key.transpose(-1,-2)) / math.sqrt(self.read_key_size) # Shape [N, heads, K_hidden, K_mem]
+        score_read = nn.Softmax(dim=3)(score_read) # Shape [N, heads, K_hidden, K_mem]
+        h_update = torch.matmul(score_read, read_value) # Shape [N, heads, K_hidden, read_value_size]
+        h_update = h_update.transpose(1, 2).flatten(start_dim=-2) # Shape [N, K_hidden, heads * read_value_size]
+        h_update = self.read_output_transform(self.read_dropout(h_update)) # Shape [N, K_hidden, D_hidden]
+
+        h_new = h + h_update # Shape [N, K_hidden, D_hidden]
+
+        return M_new, h_new
+
+
+        
