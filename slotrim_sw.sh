b2f1f7ce264f6915ea89f57dfbc81d3069ee0662
Nan <nanlin1997@outlook.com>
2022-04-19 15:07:22 +0200

    [FIX] cut half bs for sw experiment

--- slotrim_sw.sh
+++ slotrim_sw.sh
@@ -15,8 +15,10 @@ k=6
 use_sw="true"
 memory_size=100
 num_memory_slots=3
+batch_size=32
 
 python3 train_mmnist.py --experiment_name $experiment_name --cfg_json $cfg_json --core $core --should_resume $should_resume --save_frequency $save_freq \
+    --batch_size $batch_size \
     --num_slots $num_slots \
-    --k $k --num_hidden $num_hidden
+    --k $k --num_hidden $num_hidden \
     --use_sw $use_sw --memory_size $memory_size --num_memory_slots $num_memory_slots
