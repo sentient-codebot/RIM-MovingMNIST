3dfba959fa28de5dc710d91d71200bb667d95824
Nan <nanlin1997@outlook.com>
2022-04-21 19:33:12 +0200

    [ADD] bash file for SCOFF

---
+++ mmnist_scoff_basic.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+echo Running on $HOSTNAME
+# source ../../cas_env/bin/activate
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate pytorch
+
+experiment_name="MMNIST_SCOFF_basic"
+cfg_json="configs/scoff/scoff_basic.json"
+should_resume="false"
+save_freq=25
+
+python3 train_mmnist.py --experiment_name $experiment_name --cfg_json $cfg_json --should_resume $should_resume --save_frequency $save_freq 
