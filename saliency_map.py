6a7f5da10af1ad384cf047ce9c99d88ab9910254
Nan <nanlin1997@outlook.com>
2022-04-28 17:17:01 +0200

    [ADD] more flexible mmnist dataset

--- saliency_map.py
+++ saliency_map.py
@@ -6,7 +6,7 @@ from argument_parser import argument_parser
 
 import torch
 
-from datasets.MovingMNIST import MovingMNIST
+from datasets.moving_mnist_old import MovingMNIST
 from utils.visualize import SaliencyMap
 
 set_seed(2022) #Nan is stupid ciao
