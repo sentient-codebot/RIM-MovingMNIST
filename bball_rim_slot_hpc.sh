37a6050d1ad877c0ca85c6e09273eeb8557bbd61
nan <nanlin1997@outlook.com>
2022-05-04 18:04:54 +0200

    [ADD] bash for dhpc job

---
+++ bball_rim_slot_hpc.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+echo Running on $HOSTNAME
+# source ../../cas_env/bin/activate
+source ~/.bashrc
+conda activate /scratch/cristianmeo/pytorch
+
+experiment_name="BBALL_slotrim_3_6_678ball"
+cfg_json="configs/rim/rim_slot.json"
+core="RIM"
+should_resume="false"
+save_freq=10
+num_hidden=9
+num_slots=9
+k=6
+task="bball"
+dataset_dir="/scratch/cristianmeo/bouncingball"
+ball_options='transfer'
+ball_trainset="4ball"
+ball_testset="678ball"
+batch_size=32
+epochs=300
+
+
+python3 train_mmnist.py --experiment_name $experiment_name --cfg_json $cfg_json --core $core --should_resume $should_resume --save_frequency $save_freq \
+    --batch_size $batch_size --epochs $epochs\
+    --k $k --num_hidden $num_hidden --num_slots $num_slots \
+    --task $task --ball_options $ball_options --ball_trainset $ball_trainset --ball_testset $ball_testset \
+    --dataset_dir $dataset_dir
