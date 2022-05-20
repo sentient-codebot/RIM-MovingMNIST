b26c594d092c093275a88d9eaba9d8e068f301b1
Cristian Meo <cristianmeo2@gmail.com>
2022-05-20 17:58:25 +0200

    [ADD] bash files for spotlight

--- scripts/mmnist/mmnist_rim_slot_spotlight.sh
+++ scripts/mmnist/mmnist_rim_slot_spotlight.sh
@@ -6,6 +6,7 @@ conda activate pytorch
 
 experiment_name="MMNIST_SASBD_SPOTLIGHT"
 cfg_json="configs/rim/rim_slot.json"
+dataset_dir='/home/cristianmeo/movingmnist'
 core="RIM"
 should_resume="false"
 save_freq=25
@@ -14,4 +15,4 @@ num_slots=3
 k=6
 spotlight="true"
 
-python3 train_mmnist.py --experiment_name $experiment_name --cfg_json $cfg_json --core $core --should_resume $should_resume --save_frequency $save_freq --k $k --num_hidden $num_hidden --num_slots $num_slots --spotlight_bias $spotlight
+python3 train_mmnist.py --experiment_name $experiment_name --cfg_json $cfg_json --core $core --should_resume $should_resume --save_frequency $save_freq --k $k --num_hidden $num_hidden --num_slots $num_slots --spotlight_bias $spotlight --dataset_dir $dataset_dir
