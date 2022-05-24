074faed984bdad7c48faf82f4c29c3ceaffa5190
Cristian Meo <cristianmeo@login01.delftblue.tudelft.net>
2022-05-24 11:30:02 +0200

    sh files added

---
+++ scripts/sprites/sprites_sasep_rim_5_5_spot.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo Running on $HOSTNAME
+source ~/.bashrc
+conda activate pytorch
+
+
+experiment_name="SPRITES_SASEP_SPOT_5_5"
+cfg_json="configs/rim/rim_slot.json"
+core="RIM"
+should_resume="false"
+save_freq=10
+num_hidden=5
+num_slots=5
+k=5
+task="spritesmot"
+batch_size=64
+epochs=400
+decode_hidden="true"
+dataset_dir="/scratch/cristianmeo/Datasets"
+spotlight_bias="true"
+decoder_type="SEP_BASIC"
+
+
+DISABLE_ARTIFACT=1 python3 train_mmnist.py --experiment_name $experiment_name --cfg_json $cfg_json --core $core --should_resume $should_resume --save_frequency $save_freq \
+    --batch_size $batch_size --epochs $epochs\
+    --k $k --num_hidden $num_hidden --num_slots $num_slots \
+    --task $task \
+    --dataset_dir $dataset_dir \
+    --decode_hidden $decode_hidden \
+    --spotlight_bias $spotlight_bias \
+    --decoder_type $decoder_type
