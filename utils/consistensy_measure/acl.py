3788cc78461bfb3faaacb96373e1839414cb9cda
Nan <nanlin1997@outlook.com>
2022-06-23 15:58:36 +0200

    [ADD] acl fix

--- utils/consistensy_measure/acl.py
+++ utils/consistensy_measure/acl.py
@@ -3,7 +3,7 @@ import torch
 def average_consistent_length(x):
     """
     input:
-        `x`: a long tensor of shape [N, K, L]
+        `x`: a long tensor of shape [N, K, L]. batch size N, K trackers in parallel, each tracker has L frames, each frame has value 0 to K' as the object ID
     return:
         `avr_lengths`: a long tensor of shape [N, K]    
     """
@@ -19,13 +19,13 @@ def maximum_consistent_length(x):
     maximum_length, _ = torch.max(lengths, dim=-1) # Shape [N]
     return maximum_length
 
-def _to_continuous_seq_length(x):
+def _to_continuous_seq_length(x, ):
     """ Calculate continuous sequence lengths inside a normal object-id sequence
     input:
         `x`: a long tensor of shape [N, K, L]
     output:
-        `lengths`: a long tensor of shape [N, K, L]
-        `weights`: a long tensor of shape [N, K, L], signifying which elements in `lengths` are valid
+        `lengths`: a long tensor of shape [N, K, L']. L' is the maximum number of subsequences
+        `weights`: a long tensor of shape [N, K, L'], signifying which elements in `lengths` are valid
         """
     y = torch.zeros_like(x)
     for t in range(1,x.shape[2]):
@@ -33,26 +33,29 @@ def _to_continuous_seq_length(x):
         y[:,:, t] = flag * (y[:,:, t-1]+1) + (1-flag) * y[:,:, t-1]
     lengths = []
     weights = []
-    for seq_id in range(x.shape[1]):
+    for seq_id in range(torch.max(y)):
         length = (y == seq_id).sum(dim=-1) # Shape [N, K]
-        weight = (length > 0).to(torch.int) # For excluding 0 length sequence
-        length = (length - 1.)/(x.shape[2]-1) # Shape [N], normalize to 0-1, float
+        weight = (length > 1).to(torch.int) # For excluding 1 length sequence
+        length = (length - 1.)/(x.shape[2]-1.) # Shape [N], normalize to 0-1, float
         lengths.append(length)
         weights.append(weight)
-    lengths = torch.stack(lengths, dim=-1) # Shape [N, K, L]
-    weights = torch.stack(weights, dim=-1) # Shape [N, K, L]
+    lengths = torch.stack(lengths, dim=-1) # Shape [N, K, L']
+    weights = torch.stack(weights, dim=-1) # Shape [N, K, L']
     
     return lengths, weights
     
         
         
 def main():
-    dummy = torch.randint(0, 3, [4, 10])
+    # dummy = torch.randint(0, 3, [1, 1, 10])
     dummy = torch.tensor([[0, 0, 0, 0, 0, 0, 4, 4, 4, 5,]]).unsqueeze(1)
-    results = average_consistent_length(dummy).squeeze(1)
-    results = maximum_consistent_length(dummy).squeeze(1)
+    # dummy = torch.tensor([[0, 0, 0, 0, 0, 0, 0, 0, 4, 4,]]).unsqueeze(1)
+    print(dummy)
+    results = {}
+    results['acl'] = average_consistent_length(dummy).squeeze(1)
+    results['mcl'] = maximum_consistent_length(dummy).squeeze(1)
     
-    for res in results:
+    for res in results.values():
         print(f'result: {res.item()*100: .2f}', '%')
     
 if __name__ == "__main__":
